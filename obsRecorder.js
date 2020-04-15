918b801c830aef210ffbc8e22c9278acb525dcea
Andrey Novikov <envek@envek.name>
2020-04-15 11:56:38 +0300

    Fix error when OBS doesn't return any encoders, use x264 as a fallback
    Fixes https://github.com/Envek/obs-studio-node-example/issues/2

--- obsRecorder.js
+++ obsRecorder.js
@@ -54,7 +54,7 @@ function configureOBS() {
   console.debug('Configuring OBS');
   setSetting('Output', 'Mode', 'Simple');
   const availableEncoders = getAvailableValues('Output', 'Recording', 'RecEncoder');
-  setSetting('Output', 'RecEncoder', availableEncoders.slice(-1)[0]);
+  setSetting('Output', 'RecEncoder', availableEncoders.slice(-1)[0] || 'x264');
   setSetting('Output', 'FilePath', path.join(__dirname, 'videos'));
   setSetting('Output', 'RecFormat', 'mkv')
   setSetting('Output', 'VBitrate', 10000) // 10 Mbps
@@ -183,8 +183,23 @@ function setSetting(category, parameter, value) {
 
 function getAvailableValues(category, subcategory, parameter) {
   const categorySettings = osn.NodeObs.OBS_settings_getSettings(category).data;
+  if (!categorySettings) {
+    console.warn(`There is no category ${category} in OBS settings`);
+    return [];
+  }
+
   const subcategorySettings = categorySettings.find(sub => sub.nameSubCategory === subcategory);
+  if (!subcategorySettings) {
+    console.warn(`There is no subcategory ${subcategory} for OBS settings category ${category}`);
+    return [];
+  }
+
   const parameterSettings = subcategorySettings.parameters.find(param => param.name === parameter);
+  if (!parameterSettings) {
+    console.warn(`There is no parameter ${parameter} for OBS settings category ${category}.${subcategory}`);
+    return [];
+  }
+
   return parameterSettings.values.map( value => Object.values(value)[0]);
 }
 
